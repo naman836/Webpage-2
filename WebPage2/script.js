6470c2ae630a2d24e062e3446a14214dacbbe8c6
Naman Kumar Pandey <138855080+naman836@users.noreply.github.com>
2023-08-05 10:14:41 +0530

    Add files via upload

---
+++ WebPage2/script.js
@@ -0,0 +1,15 @@
+// Homepage Animation
+const header = document.querySelector('.header');
+header.classList.add('fadeIn');
+
+// Login Page Animation
+const loginSection = document.querySelector('.login-section');
+loginSection.classList.add('fadeIn');
+
+// Register Page Animation
+const registerSection = document.querySelector('.register-section');
+registerSection.classList.add('fadeIn');
+
+// Candidate Search Page Animation
+const searchSection = document.querySelector('.search-section');
+searchSection.classList.add('slideIn');
